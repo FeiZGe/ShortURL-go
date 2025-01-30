3758e97ea0d3413c79d5d4a1247aa5015a8b0986
Thanapat Nawang <thanapattt.m@gmail.com>
2025-01-30 12:35:02 +0700

    Create database.go

---
+++ config/database.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"log"
+	"short-url-service/models"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// InitDB ทำการเชื่อมต่อฐานข้อมูล SQLite
+func InitDB() {
+	var err error
+
+	// เชื่อมต่อ SQLite และสร้างไฟล์ฐานข้อมูลถ้ายังไม่มี
+	DB, err = gorm.Open(sqlite.Open("storage/urls.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// ทำการ migrate ตาราง URL
+	DB.AutoMigrate(&models.URL{})
+	log.Println("Database migrated successfully.")
+}
