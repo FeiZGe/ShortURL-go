a1c412c5e54ee99055ecc6ce3f0266d42167d128
Thanapat Nawang <thanapattt.m@gmail.com>
2025-01-30 13:23:29 +0700

    Create generator.go

---
+++ utils/generator.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"math/rand"
+	"os"
+	"time"
+)
+
+var charset = os.Getenv("CHARSET")
+
+// GenerateShortCode สุ่มสร้าง Short URL ยาว 6 ตัวอักษร
+func GenerateShortCode() string {
+	// สร้างตัว generator ที่ใช้ seed ใหม่ในแต่ละรอบการเรียก
+	src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(src)
+
+	// สุ่มรหัส 6 ตัว
+	shortCode := make([]byte, 6)
+	for i := range shortCode {
+		shortCode[i] = charset[r.Intn(len(charset))]
+	}
+
+	return string(shortCode)
+}
