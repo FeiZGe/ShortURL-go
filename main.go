50ba1c8dec8fed6607668eff98a98c3b2f108cb5
Thanapat Nawang <thanapattt.m@gmail.com>
2025-01-30 13:23:38 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/gofiber/fiber/v3"
+	
+	"github.com/FeiZGe/ShortURL-go/config"
+	"github.com/FeiZGe/ShortURL-go/routes"
 )
 
 func main() {
-	fmt.Print("Hello, guys!")
+	
+	// โหลดค่าจาก .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// เริ่มต้นเชื่อมต่อฐานข้อมูล
+	config.InitDB()
+
+	// สร้างแอป Fiber
+	app := fiber.New()
+
+	// กำหนดเส้นทาง API
+	routes.SetupRoutes(app)
+
+	// เริ่มต้นเซิร์ฟเวอร์ที่พอร์ต 3000
+	log.Println("Server is running on http://localhost:3000")
+	app.Listen(":3000")
 }
