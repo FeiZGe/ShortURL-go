595c3186eaaf62853975baeaf3d6a48bef157f7c
Thanapat Nawang <thanapattt.m@gmail.com>
2025-01-30 13:48:46 +0700

    Update: route path

--- routes/url_routes.go
+++ routes/url_routes.go
@@ -7,7 +7,7 @@ import (
 
 // SetupRoutes ตั้งค่าเส้นทาง API
 func SetupRoutes(app *fiber.App) {
-	app.Post("/shorten", handlers.CreateShortURL)  // API สำหรับย่อลิงก์
+	app.Post("/api/shorten", handlers.CreateShortURL)  // API สำหรับย่อลิงก์
 	app.Get("/:short", handlers.RedirectURL)       // API สำหรับ Redirect
-	app.Get("/stats/:short", handlers.GetClickStats) // API สำหรับดูสถิติ
+	app.Get("/api/stats/:short", handlers.GetClickStats) // API สำหรับดูสถิติ
 }
